d14e0c8bbad03af9c9c2c439439152af3146dd79
Jimmy McNamara and Kristi Hwang <kristi@>
2018-04-23 10:01:52 -0700

    run tests on Models.

---
+++ LeapYear.Tests/ModelTests/LeapYearTest.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeapYear;
+
+namespace LeapYear.Tests
+{
+  [TestClass]
+  public class LeapYearTest
+  {
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFour_True()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
+    }
+    [TestMethod]
+    public void IsLeapYear_NumberNotDivisibleByFour_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(1990));
+    }
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByOneHundred_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(1900));
+    }
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFourHundred_True()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2000));
+    }
+  }
+}
---
+++ LeapYear/Models/LeapYear.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace LeapYear
+{
+  public class LeapYear
+  {
+    public bool IsLeapYear(int year)
+    {
+      if (year % 400 == 0)
+      {
+        return true;
+      }
+      else if (year % 100 == 0)
+      {
+        return false;
+      }
+      else
+      {
+        return year % 4 == 0;
+      }
+    }
+  }
+}
